93bbde5d6df34e0327cd8726b1afdb06cba96b90
Trish Wynne <trishasone@gmail.com>
2020-10-19 12:28:20 +0100

    feat: inits server
    Co-authored-by: ephieo <ephieo@rocketmail.com>

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+
+// four get routes: experiences, laws, activites, things-to-do
