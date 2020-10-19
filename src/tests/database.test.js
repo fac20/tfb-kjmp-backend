1eef490078950a8207032690ddb6ab8b634a84a7
Joshua Hart <joshua.hart@protonmail.com>
2020-10-19 15:56:10 +0100

    Add test for getAllCountries model func Relates #10 Co-authored-by: ephieo <ephieo@rocketmail.com>

---
+++ src/tests/database.test.js
@@ -0,0 +1,25 @@
+const test = require("tape");
+const build = require("../database/build");
+const db = require("../database/connection");
+const { getAllCountries } = require("../model/countries");
+
+// test that root API route works
+
+test("get request to '/' displays all countries", t => {
+	build()
+		.then(() => {
+			getAllCountries()
+				.then(countries => {
+					t.equal(countries[0].country_name, "Afghanistan");
+					t.end();
+				})
+				.catch(error => {
+					t.error(error);
+					t.end();
+				});
+		})
+		.catch(buildError => {
+			t.error(buildError);
+			t.end();
+		});
+});
