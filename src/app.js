d12939f3c46687c6cef128fc42fadb8f23865e84
Joshua Hart <joshua.hart@protonmail.com>
2020-10-19 12:16:53 +0100

    fix landing page response 200 test Relates #10 Co-authored-by: Terrence Chung <netceer112@gmail.com>

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send("landing page");
+	// res.writeHead(200, { "content-type": "text/html" });
+	res.set("content-type", "text/html");
+	res.send("<h1>landing page</h1>");
 });
 
 module.exports = app;
--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,8 +1,32 @@
 const test = require("tape");
 const supertest = require("supertest");
-
+const app = require("../app");
 test("default test", t => {
+	// make sure dependancies are working as intended
 	let num = 2;
 	t.equal(num, 2, "should return 2");
 	t.end();
 });
+
+test("check status code is 200", t => {
+	supertest(app)
+		.get("/")
+		.expect(200)
+		.expect("content-type", "text/html; charset=utf-8")
+		.end((err, res) => {
+			t.equal(res.text, "<h1>landing page</h1>");
+			t.error(err);
+			t.end();
+		});
+});
+
+// test("check if JSON is returned", t => {
+// 	supertest(app)
+// 		.get("/brazil/activities")
+// 		.expect(200)
+// 		.expect("content-type", "application/json")
+// 		.end((err, res) => {
+// 			t.error(err);
+// 			t.end();
+// 		});
+// });
