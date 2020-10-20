6db0c55f36431612fecd9a4d1f95c52d2ed3d090
Joshua Hart <joshua.hart@protonmail.com>
2020-10-20 11:43:05 +0100

    feat: add tests for read model functions Relates #10 Co-authored-by: Terrence Chung <netceer112@gmail.com>

--- src/tests/database.test.js
+++ src/tests/database.test.js
@@ -23,3 +23,78 @@ test("get request to '/' displays all countries", t => {
 			t.end();
 		});
 });
+
+// test to ensure country-specific GET requests work for all tables
+
+test("get request for country-specific approved experiences ", t => {
+	build().then(() => {
+		getPosts(5, "experiences")
+			.then(entries => {
+				t.equal(
+					entries[0].details,
+					"Food was amazing!",
+					"Should return 'Food was amazing!'",
+				);
+				t.end();
+			})
+			.catch(error => {
+				t.error(error);
+				t.end();
+			});
+	});
+});
+
+test("get request for country-specific approved things-to-do", t => {
+	build().then(() => {
+		getPosts(5, "things_to_do")
+			.then(entries => {
+				t.equal(
+					entries[0].details,
+					"Great view!",
+					"Should return 'Great view!'",
+				);
+				t.end();
+			})
+			.catch(error => {
+				t.error(error);
+				t.end();
+			});
+	});
+});
+
+test("get request for country-specific approved businesses", t => {
+	build().then(() => {
+		getPosts(5, "businesses")
+			.then(entries => {
+				t.equal(
+					entries[0].details,
+					"Fantastic service at a great price.",
+					"Should return 'Fantastic service at a great price.'",
+				);
+				t.end();
+			})
+			.catch(error => {
+				t.error(error);
+				t.end();
+			});
+	});
+});
+
+test("get request for all unapproved entries across all tables", t => {
+	build().then(() => {
+		getUnapprovedEntries()
+			.then(entries => {
+				t.equal(
+					entries[0].details,
+					"Troll review",
+					"Should return 'Troll review'",
+				);
+				t.equal(entries[0].approved === FALSE);
+				t.end();
+			})
+			.catch(error => {
+				t.error(error);
+				t.end();
+			});
+	});
+});
