0c0f375b5b5053d294867e305321947349db7b1e
Joshua Hart <joshua.hart@protonmail.com>
2020-10-20 14:33:00 +0100

    fix: root server get test Relates #10 Co-Authored-By: Terrence Chung <netceer112@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
-const { displayAllCountries } = require("./handlers/countries");
-
 const app = require("./app");
 
 const PORT = process.env.PORT || 4000;
-
-app.get("/", displayAllCountries);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
