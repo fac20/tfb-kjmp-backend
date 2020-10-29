99c4ea62fa7b72fb75807a4ac1a7f16f50ed3fa7
Joshua Hart <joshua.hart@protonmail.com>
2020-10-29 11:15:37 +0000

    reformat auth to take token
    Co-authored-by: Terrence Chung <netceer112@gmail.com>

--- src/handlers/tableHandlers.js
+++ src/handlers/tableHandlers.js
@@ -1,4 +1,5 @@
 const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const {
 	getAllCountries,
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,24 @@
+const dotenv = require("dotenv").config;
+const SECRET = process.env.SECRET;
+const jwt = require("jsonwebtoken");
+
 function checkBasicAuth(req, res, next) {
-	const { username, password } = req.cookies;
-	if (username === "admin" && password === "password") return next();
-	else res.send("not authorised");
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		const error = new Error("Unauthorised!");
+		error.status = 400;
+		next(error);
+	}
+	try {
+		const token = authHeader.replace("Bearer ", "");
+		const tokenData = jwt.verify(token, SECRET);
+		req.user = tokenData.user;
+		next();
+	} catch (err) {
+		const error = new Error("Unauthorized");
+		error.status = 401;
+		next(error);
+	}
 }
 
 module.exports = { checkBasicAuth };
